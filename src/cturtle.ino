0f5405c2aad522d634700ca648754d96cfed1d05
Nils Florian Ziebert <uzdcz@student.kit.edu>
2025-06-23 22:08:28 +0200

    Add out of bounds check and some random adventerous lines

--- src/cturtle.ino
+++ src/cturtle.ino
@@ -53,32 +53,62 @@ struct turtle orient(struct turtle t){
   return t;
 }
 
+int outOfBounds(struct turtle t) {
+  if (t.u < uOffset || t.u >= uOffset + width || t.v < vOffset || t.v >= vOffset + height) {
+    return 1; // out of bounds
+  }
+  return 0; // within bounds
+}
+
 void setup(void)
 {
-     delay(1000);
-     u8g2.begin();
-     u8g2.setContrast(255); // set contrast to mauimum 
-     u8g2.setBusClock(400000); //400kHz I2C 
-     u8g2.setFont(u8g2_font_ncenB10_tr);
-     t.u = uCenter;
-     t.v = vCenter;
-     t.ang = 0;
+  delay(1000);
+  u8g2.begin();
+  u8g2.setContrast(255); // set contrast to mauimum 
+  u8g2.setBusClock(400000); //400kHz I2C 
+  u8g2.setFont(u8g2_font_ncenB10_tr);
+  t.u = uCenter;
+  t.v = vCenter;
+  t.ang = 0;
 }
 
 void loop(void)
 {
-//    if(i==0) {u8g2.clearBuffer();} // clear the internal memorv
-    //u8g2.drawFrame(uOffset+10, vOffset+0, width, height); //draw a frame around the border
-    t = step(t, 4);
-    t = turn(t, 1.01*i*M_PI/180);
-    
-    ++i;
-    if(i>30){
-      i = 0;
-      t = reset(t);
-      t = orient(t);
-      ++j;
-      t = turn(t, 12*j*M_PI/180);
+  while(j < 2000){
+    u8g2.clearBuffer();
+    t.u = rand() % width + uOffset-1;
+    t.v = rand() % height + vOffset-1;
+    t.ang = 0;
+    if(t.u < uCenter) {
+      if(t.v < vCenter) {
+        t.ang = M_PI_4;
+      } else {
+        t.ang = 5 * M_PI_4;
+      }
+    } else if(t.u >= uCenter) {
+      if(t.v < vCenter) {
+        t.ang = 5 * M_PI_4;
+      } else {
+        t.ang = 7 * M_PI_4;
+      }
     }
-    u8g2.sendBuffer(); // transfer internal memorv to the displav
+    t = step(t, 10);
+    u8g2.sendBuffer();
+    delay(100);
+
+    while (!outOfBounds(t)) {
+      if (outOfBounds(t)) {
+        break; // stop if out of bounds
+      }
+      float len = rand() % 5; // step length
+      float twist = rand() % 30; 
+      twist = pow(-1,rand()) *twist * M_PI / 180.0; // convert to radians
+      t = step(t, len);
+      t = turn(t, twist);
+      delay(10); // delay to visualize the steps
+      u8g2.sendBuffer();
+    }
+    delay(1000);
+    j++;
+  }
 }
